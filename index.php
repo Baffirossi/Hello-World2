ec033f798980c6e90e3e167774468480a685e847
Ian Bailey <lunardakota@gmail.com>
2013-02-28 20:02:57 -0500

    Baffirossi Index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<link href="images/favicon.ico" rel="shortcut icon">
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css" />
+
+<meta charset="UTF-8">
+<title>Baffirossi.com | Web Design &amp; Development</title>
+</head>
+
+
+<body>
+
+<div id="navbar">
+<?php include "includes/home_navbar.php" ?>
+</div>
+
+<div id="homecontent">
+<?php include "includes/home_content.php" ?>
+<div id="footer">
+<!--<hr/>-->
+</div>
+</div>
+
+</body>
+</html>
